9d3804374b8c224ecf7b0786b9907320a77e6bc4
lurbano <lurbano@earthsciweb.org>
2021-02-20 20:39:53 -0600

    test

--- webServer/static/ws-client.js
+++ webServer/static/ws-client.js
@@ -213,9 +213,10 @@ class dataGraph{
     let newx = parseFloat(dataList["t"]);
     let newy = parseFloat(dataList["x"]);
     console.log(newx, newy);
+
     let update = { x: [[newx]], y: [[newy]]};
     console.log(update);
 
-    Plotly.extendTraces(this.plot, update, {}, [0] );
+    Plotly.extendTraces(this.plot, update, [[0]] );
   }
 }
