9c95b93c32f6642d8bfe129a09a8e89fe8f11537
lurbano <lurbano@earthsciweb.org>
2021-03-27 00:31:23 -0500

    chekc

--- webServer/ledPixels.py
+++ webServer/ledPixels.py
@@ -136,6 +136,7 @@ class ledPixels:
 
     def scale(self, val):
         n = round(self.nPix * (val-self.scaleMin)/(self.scaleMax-self.scaleMin))
+        print(n)
         for i in range(n):
             self.pixels[i] = self.scaleCol
         self.pixels.show()
