9629a25d83a98e52f36181aad914fed9edc695e5
lurbano <lurbano@earthsciweb.org>
2021-03-28 23:16:48 -0500

    test

--- webServer/static/ws-client.js
+++ webServer/static/ws-client.js
@@ -409,9 +409,9 @@ class dataGraph{
     //console.log(newx, newy);
 
     //adjust for time units
-    console.log("before", this.timeUnits, newx);
+    console.log("before", this.timeUnits, newx, newy);
     newx = (this.timeUnits !== "sec") ? this.timeConvert(newy, this.timeUnits, "sec"): newx;
-    console.log("after", this.timeUnits, newx);
+    console.log("after", this.timeUnits, newx, newy);
 
 
     //adjust for temperature units
