0bccab3285d6b2bf417ee9f66eb8dc4ba6f9318b
lurbano <lurbano@earthsciweb.org>
2021-04-19 17:46:12 -0500

    CHICKENS

---
+++ webServer/chickubator.py
@@ -0,0 +1,24 @@
+from sensor_T import *
+import asyncio
+import time
+
+import RPi.GPIO as gpio
+gpio.setmode(gpio.BCM)
+
+relayPin = 26
+
+sT = sensor_T(None)
+T = sT.read()
+print(T)
+
+gpio.setup(relayPin, gpio.OUT)
+
+gpio.output(relayPin, gpio.HIGH)
+
+time.sleep(2)
+
+gpio.output(relayPin, gpio.LOW)
+
+time.sleep(2)
+
+gpio.output(relayPin, gpio.HIGH)
